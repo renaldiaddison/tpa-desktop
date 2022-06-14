027c4512171cd1d09a60b11e48fc50cca016ba15
renaldiaddison <addisonrenaldi@gmail.com>
2022-06-14 17:04:04 +0700

    sign in logic done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
+import { app } from './firebase-config';
 import './App.css';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+function signIn(e) {
+  e.preventDefault();
+  signInWithEmailAndPassword(auth, e.target.email.value, e.target.pass.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form action='' onSubmit={signIn}>
+      <input type="text" name="email" className='border'></input>
+      <input type="password" className='border'name="pass"></input>
+      <button type="submit" name="submit" className="border">submit</button>
+    </form>
+  )
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBI2-yFbd1_UfQ_CSjWwoyfdK3LHN8MDBM",
+  authDomain: "chello-478d6.firebaseapp.com",
+  projectId: "chello-478d6",
+  storageBucket: "chello-478d6.appspot.com",
+  messagingSenderId: "428823219933",
+  appId: "1:428823219933:web:0245fccad73d3dd75c1534",
+  measurementId: "G-WFMLPHF4SJ"
+};
+
+export const app = initializeApp(firebaseConfig);
