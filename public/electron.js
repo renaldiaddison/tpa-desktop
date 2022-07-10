8ab0ccf95f40c69ba0fc1c7cd435da30392f70c5
renaldiaddison <addisonrenaldi@gmail.com>
2022-07-11 02:48:31 +0700

    asd

--- public/electron.js
+++ public/electron.js
@@ -1,37 +1,41 @@
-const path = require('path');
+const path = require("path");
 
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
 
 function createWindow() {
-  
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
+    icon: path.join(__dirname, "trello.png"),
+    autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
     },
   });
 
+  win.maximize();
+  win.setFullScreen(true);
+
   win.loadURL(
     isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
   );
 
   if (isDev) {
-    win.webContents.openDevTools({ mode: 'detach' });
+    win.webContents.openDevTools({ mode: "detach" });
   }
 }
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
